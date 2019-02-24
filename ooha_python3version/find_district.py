7e7445292429c21bb76e7180af11cca4fd1a3ec3
WalterLu3 <s10130134@gmail.com>
2019-02-24 19:59:57 +0800

    Update find_district.py

--- ooha_python3version/find_district.py
+++ ooha_python3version/find_district.py
@@ -1,6 +1,5 @@
 import requests, json,dill
-from selenium import webdriver
-import time
+from xml.etree import ElementTree
 
 
 def busStopAndDistrict(busNum,city,direction):
@@ -18,47 +17,23 @@ def busStopAndDistrict(busNum,city,direction):
     for reqsjson2 in reqsjson[direction]['Stops']:
         stop[i]=(reqsjson2['StopName']['Zh_tw'],reqsjson2['StopPosition']['PositionLat'],reqsjson2['StopPosition']['PositionLon'])
         i=i+1
-        
-    #再把站牌經緯度對應到地址最後取出區
-    #這裡用了"http://itman123.com/show-google-map-latlng.html"的功能，因為google api使用要付費
     
-    driver = webdriver.Chrome() 
-    driver.get("http://itman123.com/show-google-map-latlng.html")
     result={}
-    num=0
+    ##找區
     for i in stop:
-        print(stop[i][0])
         latitude = stop[i][1]
         longtitude = stop[i][2]
-        driver.find_element_by_id("lat2").send_keys(str(latitude))
-        driver.find_element_by_id("lng2").send_keys(str(longtitude))
-        driver.find_element_by_xpath("//input[@value='經緯度查地址']").click()
-        ##要停一下不然短時間要太多會被強制跳出
-        time.sleep(2)
-        ##address存單個站牌的完整地址
-        address=driver.find_element_by_xpath("//input[@id='address2']").get_attribute("value")
-        driver.find_element_by_id("lat2").clear()
-        driver.find_element_by_id("lng2").clear()
-        
-        ##從完整地址擷取區
-        j=0
-        district=""
-        for k in address:
-            if (k=='區'):
-                district=address[j-2]+address[j-1]+k
-                break
-            j=j+1
-            
-        
-        result[i]=(stop[i][0],district)
-
-        print(address)
-        print(district)
-        num=i
+        Url="https://api.nlsc.gov.tw/other/TownVillagePointQuery/"+str(longtitude)+"/"+str(latitude)
+        print(Url)
+        a=requests.get(Url,headers=header1)
+        tree = ElementTree.fromstring(a.content)
+        result[i]=(stop[i][0],tree[3].text)
     
     with open(busNum+"_district_"+str(direction),"wb") as file:
         dill.dump(result,file)
-
+        
+   
+    
     return ('ok')
  
  ##使用例子：
